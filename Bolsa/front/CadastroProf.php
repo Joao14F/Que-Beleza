63797e03e395156619a9fc1f544f6f672b9dc246
luiza fsc <luizascrs@gmail.com>
2023-08-23 10:36:48 -0300

    mudando coisas

--- Bolsa/front/CadastroProf.php
+++ Bolsa/front/CadastroProf.php
@@ -7,6 +7,15 @@
     <title>Cadastro Profissional</title>
     <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
     <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+<<<<<<< HEAD
+    <link rel="stylesheet" type="text/css" href="css/CadastroProf.css">
+</head>
+
+<body>
+    <div class="container" style= "padding: 20px">
+        <form action="../back/cadastroProf_back.php" method="post" style= "padding: 20px"> 
+            <h1> Cadastro de Serviço </h1>           
+=======
     <style>
         body {
             background-color: #d8bfac;
@@ -28,6 +37,7 @@
 <body>
     <div class="container">
         <form action="../back/cadastroProf_back.php" method="post">            
+>>>>>>> 5c523658d271ae517605fe19ee9323c9b98ee782
             <div class="row">
             <div class="form-group">
                   <label class="areas" for="InputEmail1">Insira o titulo o seu nome</label>
@@ -53,6 +63,17 @@
                   <label class="areas" for="InputEmail1">Insira o titulo o seu endereço</label>
                   <input type="name" class="form-control" id="Nome" placeholder="endereço" name="Título">
                </div>
+<<<<<<< HEAD
+
+        </form>
+    </div>
+    <div class="container" style= "padding: 20px">
+        <form action="../back/cadastroProf_back.php" method="post" >
+            <h1> Informações sobre o Estabelecimento</h1>
+        <div class="row">
+            <div class="form-group">
+=======
+>>>>>>> 5c523658d271ae517605fe19ee9323c9b98ee782
                <div class="form-group">
                   <label class="areas" for="InputEmail1">Insira o titulo o nome do salão</label>
                   <input type="name" class="form-control" id="Nome" placeholder="nome do salão" name="Título">
