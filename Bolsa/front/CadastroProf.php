2f9e2fa1498f3f3b6f792ba1718089b9492f4a7e
luiza fsc <luizascrs@gmail.com>
2023-08-23 11:23:32 -0300

    aa

--- Bolsa/front/CadastroProf.php
+++ Bolsa/front/CadastroProf.php
@@ -13,6 +13,7 @@
 <body>
    <div class="container" style="padding: 20px">
       <form action="../back/cadastroProf_back.php" method="post" style="padding: 20px">
+<<<<<<< HEAD
          <h1> Cadastro Profissional </h2>
             <h2> Informações Pessoais </h2>
             <div class="row">
@@ -78,6 +79,58 @@
                      <div class="row">
                         <input type="submit" value="Enviar" name="submit" class="btn">
                      </div>
+=======
+         <h1> Cadastro de Serviço </h1>
+         <div class="row">
+            <div class="form-group">
+               <label class="areas" for="InputEmail1">Insira o titulo o seu nome</label>
+               <input type="name" class="form-control" id="Nome" placeholder="Nome" name="Título">
+            </div>
+            <div class="form-group">
+               <label class="areas" for="InputEmail1">Insira o seu Sobrenome</label>
+               <input type="name" class="form-control" id="Nome" placeholder="Sobrenome" name="Título">
+            </div>
+            <div class="form-group">
+               <label class="areas" for="InputEmail1">Insira o seu E-mail</label>
+               <input type="name" class="form-control" id="Nome" placeholder="E-mail" name="Título">
+            </div>
+            <div class="form-group">
+               <label class="areas" for="InputEmail1">Insira o titulo o seu telefone</label>
+               <input type="name" class="form-control" id="Nome" placeholder="telefone" name="Título">
+            </div>
+            <div class="form-group">
+               <label class="areas" for="InputEmail1">Insira o titulo o seu senha</label>
+               <input type="name" class="form-control" id="Nome" placeholder="senha" name="Título">
+            </div>
+            <div class="form-group">
+               <label class="areas" for="InputEmail1">Insira o titulo o seu endereço</label>
+               <input type="name" class="form-control" id="Nome" placeholder="endereço" name="Título">
+            </div>
+
+      </form>
+   </div>
+   <div class="container" style="padding: 20px">
+      <form action="../back/cadastroProf_back.php" method="post">
+         <h1> Informações sobre o Estabelecimento</h1>
+         <div class="row">
+            <div class="form-group">
+               <div class="form-group">
+                  <label class="areas" for="InputEmail1">Insira o titulo o nome do salão</label>
+                  <input type="name" class="form-control" id="Nome" placeholder="nome do salão" name="Título">
+               </div>
+               <div class="form-group">
+                  <label class="areas" for="InputEmail1">Insira o titulo o seu especialização</label>
+                  <input type="name" class="form-control" id="Nome" placeholder="especialização" name="Título">
+               </div>
+               <div class="form-group">
+                  <label class="areas" for="InputEmail1">Insira o titulo sua experiencia</label>
+                  <input type="name" class="form-control" id="Nome" placeholder="experiencia" name="Título">
+               </div>
+            </div>
+            <div class="row">
+               <input type="submit" value="Enviar" name="submit" class="btn">
+            </div>
+>>>>>>> 75158f5fee20d38f90e4ef7108d471a49e696131
       </form>
    </div>
 
