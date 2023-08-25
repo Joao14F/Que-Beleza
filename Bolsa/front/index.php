2032299bde8f925ef5d0fa1edbecae0bd0c6761e
raissa <hendlerf.raissa@gmail.com>
2023-08-25 16:24:50 -0300

    Update index.php
    teste

--- Bolsa/front/index.php
+++ Bolsa/front/index.php
@@ -44,24 +44,42 @@
             <br>
         <div class="row">
             <div class="col-3 col-sm-3 col-md-3"></div>
-            <button class="col-3 col-sm-3 col-md-3"><a href="">Profissional</a></button>
-            <button class="col-3 col-sm-3 col-md-3"><a href="">Usuário</a></button>
+            <button class="col-3 col-sm-3 col-md-3" id="prof"><a href="">Profissional</a></button>
+            <button class="col-3 col-sm-3 col-md-3" id="user"><a href="">Usuário</a></button>
             <div class="col-3 col-sm-3 col-md-3"></div>
         </div>
         <br>
         <div class="row">
             <div class="col-1 col-sm-1 col-md-1"></div>
-            <button class="col-10 col-sm-10 col-md-10"><a href="">Login</a></button>
+            <button class="col-10 col-sm-10 col-md-10" id="login"><a href="#">Login</a></button>
             <div class="col-1 col-sm-1 col-md-1"></div>
         </div>
         <div class="row">
         <div class="col-1 col-sm-1 col-md-1"></div>
-            <button class="col-10 col-sm-10 col-md-10"><a href="">Cadastro</a></button>
+            <button class="col-10 col-sm-10 col-md-10" id="cadastro"><a href="#">Cadastro</a></button>
             <div class="col-1 col-sm-1 col-md-1"></div>
         </div>
         <br>
         <br>
         </form>
     </div>
+    <script>
+        function setDefaultLoginCadastro() {
+            document.getElementById('login').innerHTML = '<a href="loginUsu.php">Login</a>';
+            document.getElementById('cadastro').innerHTML = '<a href="CadastroUsu.php">Cadastro</a>';
+        }
+
+        document.getElementById('prof').addEventListener('click', function(event) {
+            event.preventDefault();
+            document.getElementById('login').innerHTML = '<a href="loginProf.php">Login</a>';
+            document.getElementById('cadastro').innerHTML = '<a href="CadastroProf.php">Cadastro</a>';
+        });
+
+        document.getElementById('user').addEventListener('click', function(event) {
+            event.preventDefault();
+            setDefaultLoginCadastro();
+        });
+        setDefaultLoginCadastro();
+    </script>
 </body>
 </html>
