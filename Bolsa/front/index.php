1bfa806cc139454b7e3d64cb95b9da18be8f4025
luiza fsc <luizascrs@gmail.com>
2023-09-13 17:20:17 -0300

    index

--- Bolsa/front/index.php
+++ Bolsa/front/index.php
@@ -1,63 +1,72 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tipo de conta</title>
+    <title>Que Beleza</title>
     <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
     <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" type="text/css" href="css/index.css">
 </head>
+
 <body>
     <div class="container">
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <form>
-            <br>
-        <div class="row">
-            <div class="col-3 col-sm-3 col-md-3"></div>
-            <button class="col-3 col-sm-3 col-md-3" id="prof"><a href="">Profissional</a></button>
-            <button class="col-3 col-sm-3 col-md-3" id="user"><a href="">Usuário</a></button>
-            <div class="col-3 col-sm-3 col-md-3"></div>
+        <div class="bola">
         </div>
-        <br>
-        <div class="row">
-            <div class="col-1 col-sm-1 col-md-1"></div>
-            <button class="col-10 col-sm-10 col-md-10" id="login"><a href="#">Login</a></button>
-            <div class="col-1 col-sm-1 col-md-1"></div>
-        </div>
-        <div class="row">
-        <div class="col-1 col-sm-1 col-md-1"></div>
-            <button class="col-10 col-sm-10 col-md-10" id="cadastro"><a href="#">Cadastro</a></button>
-            <div class="col-1 col-sm-1 col-md-1"></div>
-        </div>
-        <br>
-        <br>
-        </form>
-    </div>
-    <script>
-        function setDefaultLoginCadastro() {
-            document.getElementById('login').innerHTML = '<a href="login.php">Login</a>';
-            document.getElementById('cadastro').innerHTML = '<a href="CadastroUsu.php">Cadastro</a>';
-        }
 
-        document.getElementById('prof').addEventListener('click', function(event) {
-            event.preventDefault();
-            document.getElementById('login').innerHTML = '<a href="loginProf.php">Login</a>';
-            document.getElementById('cadastro').innerHTML = '<a href="CadastroProf.php">Cadastro</a>';
-        });
 
-        document.getElementById('user').addEventListener('click', function(event) {
-            event.preventDefault();
+        <div class="bola2">
+
+            <div class="card">
+                <br>
+                <h1> Bem vindo (a)! </h1>
+                <br>
+                <br>
+                <div class="checkbox-wrapper-35" style="margin-left: 85px; padding-bottom: 40px">
+                    <input value="private" name="switch" id="switch" type="checkbox" class="switch">
+                    <label for="switch">
+                        <span class="switch-x-text"> Sou </span>
+                        <span class="switch-x-toggletext">
+                            <span class="switch-x-unchecked"><span class="switch-x-hiddenlabel">Unchecked: </span>Cliente</span>
+                            <span class="switch-x-checked"><span class="switch-x-hiddenlabel">Checked: </span>Profissional</span>
+                        </span>
+                    </label>
+                </div>
+
+                <button class="cta" id="login">
+                    <span class="hover-underline-animation"> Login </span>
+                    <svg viewBox="0 0 46 16" height="10" width="30" xmlns="http://www.w3.org/2000/svg" id="arrow-horizontal">
+                        <path transform="translate(30)" d="M8,0,6.545,1.455l5.506,5.506H-30V9.039H12.052L6.545,14.545,8,16l8-8Z" data-name="Path 10" id="Path_10"></path>
+                    </svg>
+                </button>
+                <br>
+                <button class="cta" id="cadastro">
+                    <span class="hover-underline-animation"> Cadastro </span>
+                    <svg viewBox="0 0 46 16" height="10" width="30" xmlns="http://www.w3.org/2000/svg" id="arrow-horizontal">
+                        <path transform="translate(30)" d="M8,0,6.545,1.455l5.506,5.506H-30V9.039H12.052L6.545,14.545,8,16l8-8Z" data-name="Path 10" id="Path_10"></path>
+                    </svg>
+                </button>
+            </div>
+        </div>
+        <script>
+            function setDefaultLoginCadastro() {
+                document.getElementById('login').innerHTML = '<a href="login.php">Login</a>';
+                document.getElementById('cadastro').innerHTML = '<a href="CadastroUsu.php">Cadastro</a>';
+            }
+
+            document.getElementById('prof').addEventListener('click', function(event) {
+                event.preventDefault();
+                document.getElementById('login').innerHTML = '<a href="loginProf.php">Login</a>';
+                document.getElementById('cadastro').innerHTML = '<a href="CadastroProf.php">Cadastro</a>';
+            });
+
+            document.getElementById('user').addEventListener('click', function(event) {
+                event.preventDefault();
+                setDefaultLoginCadastro();
+            });
             setDefaultLoginCadastro();
-        });
-        setDefaultLoginCadastro();
-    </script>
+        </script>
 </body>
+
 </html>
