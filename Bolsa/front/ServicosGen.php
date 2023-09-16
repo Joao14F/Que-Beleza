ddc180369e87942c009d8393e8e809e155ad68cb
João Francisco Bratti <jfranciscobratti@gmail.com>
2023-09-16 16:52:23 -0300

    pouquinha coisa

--- Bolsa/front/ServicosGen.php
+++ Bolsa/front/ServicosGen.php
@@ -9,7 +9,7 @@
   <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
   <link rel="stylesheet" type="text/css" href="css/ServicosGen.css">
   <style>
-    
+
   </style>
 </head>
 
@@ -43,28 +43,22 @@
       <p class="col-12 col-sm-12 col-md-12"></p>
     </div>
 
+    <div class="row">    
     <aside>
-    <dl>
-    <dt>Nome do serviço 1</dt>
-    <dd>Descrição do serviço 1</dd>
-    
-    <dt>Nome do serviço 2</dt>
-    <dd>Descrição do serviço 2</dd>
-    
-  </dl>
-    </aside>
+    <div class="col-12 col-sm-12 col-md-12">
+      <dl>
+        <dt>Nome do serviço 1</dt>
+        <dd>Descrição do serviço 1</dd>
 
-    <footer>
-      <div class="row">
-        <div class="col-1 col-sm-1"></div>
-        <button class="col-2 col-sm-2"><a href="">....</a></button>
-        <div class="col-2 col-sm-2"></div>
-        <button class="col-2 col-sm-2"><a href="principal.php">Principal</a></button>
-        <div class="col-2 col-sm-2"></div>
-        <button class="col-2 col-sm-2"><a href="conta.php">Conta</a></button>
-        <div class="col-1 col-sm-1"></div>
-      </div>
-    </footer>
+        <dt>Nome do serviço 2</dt>
+        <dd>Descrição do serviço 2</dd>
+
+      </dl>
+    </aside>
+</div>
+    </div>
+
+
   </div>
 </body>
 
---
+++ Bolsa/front/rodape.php
@@ -0,0 +1,11 @@
+<footer>
+            <div class="row">
+                <div class="col-1 col-sm-1"></div>
+                <button class="col-2 col-sm-2"><a href="">....</a></button>
+                <div class="col-2 col-sm-2"></div>
+                <button class="col-2 col-sm-2"><a href="principal.php">principal</a></button>
+                <div class="col-2 col-sm-2"></div>
+                <button class="col-2 col-sm-2"><a href="conta.php">conta</a></button>
+                <div class="col-1 col-sm-1"></div>
+            </div>
+        </footer>
