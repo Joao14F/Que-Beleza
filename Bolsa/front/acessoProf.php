ac6ef6105bb373db95e3737883b9bb599517b2fc
João Francisco Bratti <jfranciscobratti@gmail.com>
2023-09-27 21:26:51 -0300

    socorro

--- Bolsa/front/acessoProf.php
+++ Bolsa/front/acessoProf.php
@@ -1,82 +1,32 @@
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tela de início</title>
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            background-color: #e5e5e5;
-        }
+    <title>Conta</title>
 
-        .salao img {
+    <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
+    <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+    <style>
+        img {
             margin: -4px 0px;
         }
 
-        .buttonunhas {
-            background-color: #6a66a3;
-            color: #fff;
-            border: none;
-            padding: 5px;
-            text-decoration: none;
-            cursor: pointer;
-            border-radius: 20px;
-            margin: 5px;
-        }
-
-        .serv {
-            filter: grayscale(100%);
-            filter: blur(5px);
-            filter: opacity(30%);
-            border-radius: 20px;
-        }
-
-        .input {
-            border-radius: 12px;
-            border: 1.5px solid lightgrey;
-            outline: none;
-            transition: all 0.3s cubic-bezier(0.19, 1, 0.22, 1);
-            box-shadow: 0px 0px 20px -18px;
-        }
-
-        .input:hover {
-            border: 2px solid lightgrey;
-            box-shadow: 0px 0px 20px -17px;
-        }
-
-        .input:active {
-            transform: scale(0.95);
-        }
-
-        .input:focus {
-            border: 2px solid grey;
-        }
-
-        p {
-            font-weight: bold;
-        }
-
-        .center-button {
-            text-align: center;
-        }
     </style>
 </head>
 
 <body>
     <div class="container">
         <div class="row salao">
-            <div class="col-12">
-                <img src="imagens/unhas.jpeg" alt="imagens fictícias" class="col-12 col-sm-12 col-md-12">
-            </div>
+            <img src="imagens/unhas.jpeg" alt="imagnes ficticea" class="col-12 col-sm-12 col-md-12 gx-0">
         </div>
-        <div class="row justify-content-center">
+        <div class="row">
             <div class="col-12">
                 <input class="input" name="text" type="text" placeholder="Pesquise serviços...">
             </div>
         </div>
-        <div class="row center-button">
+        <div class="row">
             <div class="col-10">
                 <button class="buttonunhas">
                     <a href="servicos.php">
@@ -86,7 +36,7 @@
                 </button>
             </div>
         </div>
-        <div class="row center-button">
+        <div class="row">
             <div class="col-10">
                 <button class="buttonunhas">
                     <a href="servicos.php">
@@ -96,7 +46,7 @@
                 </button>
             </div>
         </div>
-        <div class="row center-button">
+        <div class="row">
             <div class="col-10">
                 <button class="buttonunhas">
                     <a href="servicos.php">
