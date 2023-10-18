629816318f399914871fb341bc4daffe42d09421
luiza fsc <luizascrs@gmail.com>
2023-10-17 21:31:31 -0300

    area principal pronta

--- Bolsa/front/principal2.php
+++ Bolsa/front/principal2.php
@@ -7,6 +7,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
     <link rel="stylesheet" href="css/principal2.css">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.css" />
     <title> Tela de início </title>
 
 </head>
@@ -15,11 +17,10 @@
     <header class="header">
         <a href="#" class="logo"><i class="fa-solid fa-spa"></i> Quê Beleza</a>
         <nav class="navbar">
-            <a href="#servicos"> serviços </a>
-            <a href="#review"> review </a>
-            <a href="#servicos"> serviços </a>
-            <a href="#review"> review </a>
-            
+            <a href="#features"> Estabelecimentos </a>
+            <a href="#products"> Serviços </a>
+            <a href="#review"> Review </a>
+
         </nav>
 
         <div class="icons">
@@ -37,24 +38,270 @@
     </header>
     <section class="home" id="home">
 
-    <div class="content">
-        <h3>fresh and <span>organic</span> products</h3>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquid illo magni consequuntur aperiam impedit, in autem quas ipsum deleniti labore odio atque voluptatem corrupti nihil exercitationem maxime animi ducimus porro.</p>
-        <a href="#" class="btn"> shop now</a>
-    </div>
+        <div class="content">
+            <h3>fresh and <span>organic</span> products</h3>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquid illo magni consequuntur aperiam impedit, in autem quas ipsum deleniti labore odio atque voluptatem corrupti nihil exercitationem maxime animi ducimus porro.</p>
+            <a href="#" class="btn"> shop now</a>
+        </div>
 
     </section>
     <section class="features" id="features">
         <h1 class="heading"> our <span>features</span></h1>
         <div class="box-container">
+
             <div class="box">
-                <img src="" alt="">
+                <img src="../front/fotos/salao.png" alt="" width="200px" height="150px">
+                <h3> fresh and organic </h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti placeat optio cumque itaque sit beatae, dicta, nemo necessitatibus commodi, voluptas consectetur asperiores ratione ipsa. Voluptatum eaque nemo qui aspernatur nihil.</p>
+                <a href="#" class="btn"> read more </a>
+            </div>
+
+            <div class="box">
+                <img src="../front/fotos/salao.png" alt="" width="200px" height="150px">
+                <h3> fresh and organic </h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti placeat optio cumque itaque sit beatae, dicta, nemo necessitatibus commodi, voluptas consectetur asperiores ratione ipsa. Voluptatum eaque nemo qui aspernatur nihil.</p>
+                <a href="#" class="btn"> read more </a>
+            </div>
+
+            <div class="box">
+                <img src="../front/fotos/salao.png" alt="" width="200px" height="150px">
+                <h3> fresh and organic </h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti placeat optio cumque itaque sit beatae, dicta, nemo necessitatibus commodi, voluptas consectetur asperiores ratione ipsa. Voluptatum eaque nemo qui aspernatur nihil.</p>
+                <a href="#" class="btn"> read more </a>
             </div>
         </div>
     </section>
+
+    <section class="products" id="products">
+        <h1 class="heading">our <span> products </span> </h1>
+        <div class="swiper product-slider">
+            <div class="swiper-wrapper">
+                <div class="swiper-slide box">
+                    <img src="../front/fotos/produtos.png" alt="" width="200px" height="150px">
+                    <h3> Maquiagem </h3>
+                    <div class="price"> R$100,00 - R$180,00</div>
+                    <div class="stars">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star-half-alt"></i>
+                    </div>
+                    <a href="#" class="btn">Marcar Horário</a>
+                </div>
+
+                <div class="swiper-slide box">
+                    <img src="../front/fotos/produtos.png" alt="" width="200px" height="150px">
+                    <h3> Maquiagem </h3>
+                    <div class="price"> R$100,00 - R$180,00</div>
+                    <div class="stars">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star-half-alt"></i>
+                    </div>
+                    <a href="#" class="btn">Marcar Horário</a>
+                </div>
+
+                <div class="swiper-slide box">
+                    <img src="../front/fotos/produtos.png" alt="" width="200px" height="150px">
+                    <h3> Maquiagem </h3>
+                    <div class="price"> R$100,00 - R$180,00</div>
+                    <div class="stars">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star-half-alt"></i>
+                    </div>
+                    <a href="#" class="btn">Marcar Horário</a>
+                </div>
+
+                <div class="swiper-slide box">
+                    <img src="../front/fotos/produtos.png" alt="" width="200px" height="150px">
+                    <h3> Maquiagem </h3>
+                    <div class="price"> R$100,00 - R$180,00</div>
+                    <div class="stars">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star-half-alt"></i>
+                    </div>
+                    <a href="#" class="btn">Marcar Horário</a>
+                </div>
+
+                <div class="swiper-slide box">
+                    <img src="../front/fotos/produtos.png" alt="" width="200px" height="150px">
+                    <h3> Maquiagem </h3>
+                    <div class="price"> R$100,00 - R$180,00</div>
+                    <div class="stars">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star-half-alt"></i>
+                    </div>
+                    <a href="#" class="btn">Marcar Horário</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="swiper product-slider">
+            <div class="swiper-wrapper">
+                <div class="swiper-slide box">
+                    <img src="../front/fotos/produtos.png" alt="" width="200px" height="150px">
+                    <h3> Maquiagem </h3>
+                    <div class="price"> R$100,00 - R$180,00</div>
+                    <div class="stars">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star-half-alt"></i>
+                    </div>
+                    <a href="#" class="btn">Marcar Horário</a>
+                </div>
+
+                <div class="swiper-slide box">
+                    <img src="../front/fotos/produtos.png" alt="" width="200px" height="150px">
+                    <h3> Maquiagem </h3>
+                    <div class="price"> R$100,00 - R$180,00</div>
+                    <div class="stars">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star-half-alt"></i>
+                    </div>
+                    <a href="#" class="btn">Marcar Horário</a>
+                </div>
+                
+                <div class="swiper-slide box">
+                    <img src="../front/fotos/produtos.png" alt="" width="200px" height="150px">
+                    <h3> Maquiagem </h3>
+                    <div class="price"> R$100,00 - R$180,00</div>
+                    <div class="stars">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star-half-alt"></i>
+                    </div>
+                    <a href="#" class="btn">Marcar Horário</a>
+                </div>
+
+                <div class="swiper-slide box">
+                    <img src="../front/fotos/produtos.png" alt="" width="200px" height="150px">
+                    <h3> Maquiagem </h3>
+                    <div class="price"> R$100,00 - R$180,00</div>
+                    <div class="stars">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star-half-alt"></i>
+                    </div>
+                    <a href="#" class="btn">Marcar Horário</a>
+                </div>
+
+                <div class="swiper-slide box">
+                    <img src="../front/fotos/produtos.png" alt="" width="200px" height="150px">
+                    <h3> Maquiagem </h3>
+                    <div class="price"> R$100,00 - R$180,00</div>
+                    <div class="stars">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star-half-alt"></i>
+                    </div>
+                    <a href="#" class="btn">Marcar Horário</a>
+                </div>
+            </div>
+        </div>
+
+    </section>
+
+    <section class="review" id="review">
+        <h1 class="heading"> Avaliação dos <span> clientes </span> </h1>
+        <div class="swiper review-slider">
+            <div class="swiper-wrapper">
+                <div class="swiper-slide box">
+                    <img src="../front/fotos/user.png" alt="" width="50px" height="50px">
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum odit dolorem quae, eveniet consectetur porro dicta laudantium ad tenetur quas voluptatem! Odio amet, rerum delectus tempora quam aut reprehenderit temporibus!</p>
+                    <h3>bratti</h3>
+                    <div class="stars">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star-half-alt"></i>
+                    </div>
+                </div>
+
+                <div class="swiper-slide box">
+                    <img src="../front/fotos/user.png" alt=""width="50px" height="50px">
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum odit dolorem quae, eveniet consectetur porro dicta laudantium ad tenetur quas voluptatem! Odio amet, rerum delectus tempora quam aut reprehenderit temporibus!</p>
+                    <h3>bratti</h3>
+                    <div class="stars">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star-half-alt"></i>
+                    </div>
+                </div>
+                
+                <div class="swiper-slide box">
+                    <img src="../front/fotos/user.png" alt="" width="50px" height="50px">
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum odit dolorem quae, eveniet consectetur porro dicta laudantium ad tenetur quas voluptatem! Odio amet, rerum delectus tempora quam aut reprehenderit temporibus!</p>
+                    <h3>bratti</h3>
+                    <div class="stars">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star-half-alt"></i>
+                    </div>
+                </div>
+                
+                <div class="swiper-slide box">
+                    <img src="../front/fotos/user.png" alt="" width="50px" height="50px">
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum odit dolorem quae, eveniet consectetur porro dicta laudantium ad tenetur quas voluptatem! Odio amet, rerum delectus tempora quam aut reprehenderit temporibus!</p>
+                    <h3>bratti</h3>
+                    <div class="stars">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star-half-alt"></i>
+                    </div>
+                </div>
+                <div class="swiper-slide box">
+                    <img src="../front/fotos/user.png" alt="" width="50px" height="50px">
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum odit dolorem quae, eveniet consectetur porro dicta laudantium ad tenetur quas voluptatem! Odio amet, rerum delectus tempora quam aut reprehenderit temporibus!</p>
+                    <h3>bratti</h3>
+                    <div class="stars">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star-half-alt"></i>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
+
+
     <?php
     require('rodape.php');
     ?>
+    <script src="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.js"></script>
+
     <script src="scripts.js"></script>
 
 </body>
--- Bolsa/front/scripts.js
+++ Bolsa/front/scripts.js
@@ -7,19 +7,64 @@ document.querySelector('#search-btn').onclick = () => {
 let Home = document.querySelector('.home-form');
 
 document.querySelector('#home-btn').onclick = () => {
-    window.location.href= "index.php";
+    window.location.href = "index.php";
 }
 
 document.querySelector('#calendar-btn').onclick = () => {
-    window.location.href= "calendario.php";
+    window.location.href = "calendario.php";
+}
+
+document.querySelector('#user-btn').onclick = () => {
+    window.location.href = "conta.php";
 }
 
 let navbar = document.querySelector('.navbar');
 document.querySelector('#menu-btn').onclick = () => {
     navbar.classList.toggle('active');
 }
-window.onscroll= () => {
+window.onscroll = () => {
     navbar.classList.remove('active');
-    searchForm.classList.toggle('active');
 
 }
+
+var swiper = new Swiper(".product-slider", {
+    loop:true,
+    spaceBetween: 20,
+    autoplay: {
+        delay:7500,
+        disableOnInteraction: false,
+    },
+    centeredSlides: true, 
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1020: {
+            slidesPerView: 3,
+        },
+    },
+});
+
+var swiper = new Swiper(".review-slider", {
+    loop:true,
+    spaceBetween: 20,
+    autoplay: {
+        delay:7500,
+        disableOnInteraction: false,
+    },
+    centeredSlides: true, 
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1020: {
+            slidesPerView: 3,
+        },
+    },
+});
