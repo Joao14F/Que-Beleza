7b4894ae8a95ac1834bd190f540fbff2a9a860a2
João Francisco Bratti <jfranciscobratti@gmail.com>
2023-11-28 23:19:44 -0300

    coisas

--- Bolsa/front/loginProf.php
+++ Bolsa/front/loginProf.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,23 +9,32 @@
     <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" type="text/css" href="css/loginProf.css">
 </head>
-<body>
+
+<body class="d-flex align-items-center">
     <div class="container">
-        <form action="../back/loginProf_back.php" method="POST">
         <div class="row">
-               <div class="form-group">
-                  <label class="areas" for="InputEmail1">Insira o seu E-mail</label>
-                  <input type="email" class="form-control" id="Nome" placeholder="E-mail" name="email" required>
-               </div>
-               <div class="form-group">
-                  <label class="areas" for="InputEmail1">Insira o titulo o seu senha</label>
-                  <input type="password" class="form-control" id="Nome" placeholder="senha" name="senha" required>
-               </div>
+            <div class="col-12 text-center">
+                <h3>Logue-se</h3>
+            </div>
+        </div>
+        <form action="../back/loginProf_back.php" method="POST">
+            <div class="row">
+                <div class="form-group">
+                    <label class="areas" for="InputEmail1">Insira o seu E-mail</label>
+                    <input type="email" class="form-control" id="Nome" placeholder="E-mail" name="email" required>
+                </div>
+            </div>
+            <div class="row">
+                <div class="form-group">
+                    <label class="areas" for="InputEmail1">Insira o titulo o seu senha</label>
+                    <input type="password" class="form-control" id="Nome" placeholder="senha" name="senha" required>
+                </div>
+            </div>
             <div class="row">
                 <input type="submit" value="Enviar" name="submit" class="btn">
             </div>
-    </div>
         </form>
     </div>
 </body>
+
 </html>
--- Bolsa/front/loginUsu.php
+++ Bolsa/front/loginUsu.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -9,23 +10,32 @@
     <link rel="stylesheet" type="text/css" href="css/loginUsu.css">
 </head>
 </head>
-<body>
+
+<body class="d-flex align-items-center">
     <div class="container">
-        <form action="../back/login_back.php" method="POST">
         <div class="row">
-               <div class="form-group">
-                  <label class="areas" for="InputEmail1">Insira o seu E-mail</label>
-                  <input type="email" class="form-control" id="Nome" placeholder="E-mail" name="email" require>
-               </div>
-               <div class="form-group">
-                  <label class="areas" for="InputEmail1">Insira o titulo o seu senha</label>
-                  <input type="password" class="form-control" id="Nome" placeholder="senha" name="senha" require>
-               </div>
+            <div class="col-12 text-center">
+                <h3>Logue-se</h3>
+            </div>
+        </div>
+        <form action="../back/login_back.php" method="POST">
+            <div class="row">
+                <div class="form-group">
+                    <label class="areas" for="InputEmail1">Insira o seu E-mail</label>
+                    <input type="email" class="form-control" id="Nome" placeholder="E-mail" name="email" required>
+                </div>
+            </div>
+            <div class="row">
+                <div class="form-group">
+                    <label class="areas" for="InputEmail1">Insira o titulo o seu senha</label>
+                    <input type="password" class="form-control" id="Nome" placeholder="senha" name="senha" required>
+                </div>
+            </div>
             <div class="row">
                 <input type="submit" value="Enviar" name="submit" class="btn">
             </div>
-    </div>
         </form>
     </div>
 </body>
+
 </html>
